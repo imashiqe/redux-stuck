44fee348d267460ce959ae3876e38a2ed2da22b3
Ashiqur Rahman <imashiqe@gmail.com>
2024-08-19 14:02:33 +0600

    redux stuck

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import "./index.css";
+
+function App() {
+  return (
+    <div className="h-screen w-full flex justify-center items-center">
+      <div className="flex">
+        <button className="px-3 py-2 rounded-md bg-green-500  text-xl font-semibold">
+          Increment{" "}
+        </button>
+        <h1 className="text-3xl mx-10">0</h1>
+        <button className="px-3 py-2 rounded-md bg-red-500 text-xl font-semibold">
+          Decrement{" "}
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
