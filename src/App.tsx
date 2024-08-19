541871e42913e7ae64ff0152b7374f4346d4e509
Ashiqur Rahman <imashiqe@gmail.com>
2024-08-19 16:04:27 +0600

    upgrade with typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 import { useDispatch, useSelector } from "react-redux";
 import "./index.css";
-import { decrement, increment } from "./redux/features/counterSlice";
+import {
+  decrement,
+  increment,
+  incrementByValue,
+} from "./redux/features/counterSlice";
+import { RootState } from "./redux/store";
 
 function App() {
-  const { count } = useSelector((state) => state.counter);
+  const { count } = useSelector((state: RootState) => state.counter);
   const dispatch = useDispatch();
   return (
     <div className="h-screen w-full flex justify-center items-center">
       <div className="flex border-purple-300 rounded-md bg-slate-50 p-10">
+        <button
+          onClick={() => dispatch(incrementByValue(5))}
+          className="px-3 py-2 rounded-md bg-green-500  text-xl font-semibold"
+        >
+          Increment By 5
+        </button>
+
         <button
           onClick={() => dispatch(increment())}
           className="px-3 py-2 rounded-md bg-green-500  text-xl font-semibold"
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -6,3 +6,9 @@ export const store = configureStore({
     counter: counterSlice,
   },
 });
+
+console.log(store.getState());
+
+export type RootState = ReturnType<typeof store.getState>;
+
+export type AppDispatch = typeof store.dispatch;
