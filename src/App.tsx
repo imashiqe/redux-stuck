26d0340e71f4bfa45b8a2e42509d38bf26cccd35
Ashiqur Rahman <imashiqe@gmail.com>
2024-08-19 14:14:53 +0600

    store configuration

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./index.css";
 function App() {
   return (
     <div className="h-screen w-full flex justify-center items-center">
-      <div className="flex">
+      <div className="flex border-purple-300 rounded-md bg-slate-50 p-10">
         <button className="px-3 py-2 rounded-md bg-green-500  text-xl font-semibold">
           Increment{" "}
         </button>
