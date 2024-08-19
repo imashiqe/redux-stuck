7d7f65501fcda827949589accb4081fafda05e3e
Ashiqur Rahman <imashiqe@gmail.com>
2024-08-19 15:09:42 +0600

    increment decrement update

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
+import { useDispatch, useSelector } from "react-redux";
 import "./index.css";
+import { decrement, increment } from "./redux/features/counterSlice";
 
 function App() {
+  const { count } = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
   return (
     <div className="h-screen w-full flex justify-center items-center">
       <div className="flex border-purple-300 rounded-md bg-slate-50 p-10">
-        <button className="px-3 py-2 rounded-md bg-green-500  text-xl font-semibold">
-          Increment{" "}
+        <button
+          onClick={() => dispatch(increment())}
+          className="px-3 py-2 rounded-md bg-green-500  text-xl font-semibold"
+        >
+          Increment
         </button>
-        <h1 className="text-3xl mx-10">0</h1>
-        <button className="px-3 py-2 rounded-md bg-red-500 text-xl font-semibold">
-          Decrement{" "}
+        <h1 className="text-3xl mx-10">{count}</h1>
+        <button
+          onClick={() => dispatch(decrement())}
+          className="px-3 py-2 rounded-md bg-red-500 text-xl font-semibold"
+        >
+          Decrement
         </button>
       </div>
     </div>
